4e11d49ae1e51e806e4eb4621176862da8189dc3
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-06-20 23:18:48 -0400

    commented out bugs i made

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const thoughtRoutes = require('./thoughtRoutes');
 const userRoutes = require('./userRoutes');
-const apiRoutes = require('./api');
+// const apiRoutes = require('./api');
 
-router.use('/api', apiRoutes);
+// router.use('/api', apiRoutes);
 router.use('/thoughts', thoughtRoutes);
 router.use('/users', userRoutes);
 router.use((req, res) => res.send('Wrong route!'));
