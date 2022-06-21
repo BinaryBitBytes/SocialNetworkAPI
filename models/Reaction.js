bc6fcdf37f96c6ab3d0366a6e280bb5cd4cb562c
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-06-20 22:24:12 -0400

    found a fault in my models/schema relating to friend.js realizing it needed to get corrected to a reaction. renamed the Friend.js file to Reaction.js and made the changes to reflect the Reaction.js

---
+++ models/Reaction.js
@@ -0,0 +1,26 @@
+const { Schema, Types } = require('mongoose');
+
+//create a reaction model schema
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+);
+
+module.exports = reactionSchema;
