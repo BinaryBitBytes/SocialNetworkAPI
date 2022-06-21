68a71e4ef95b050672ea5922403e33cb1521633c
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-06-20 22:30:42 -0400

    updated thoughtRoute.js found i was missing a few router routes.

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -16,6 +16,14 @@ router
 .put(updateThought)
 .delete(deleteThought);
 
+router
+  .route('/:thoughtId/reactions')
+  .post(createReaction);
+
+router
+.route('/:thoughtId/reactions/:reactionId')
+.delete(deleteReaction);
+
 module.exports = router;
 module.exports = thoughtRoutes;
 
